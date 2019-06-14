a9150ace9d37599293ae27fe5bfbe1e35b6f6d2f
nguoicuagiono <51014173+nguoicuagiono@users.noreply.github.com>
2019-06-14 13:21:00 +0700

    Add files via upload

---
+++ js/timKiem.js
@@ -0,0 +1,32 @@
+var database = [
+    ["yêu anh hơn Cả tử thần", "sachyeuanh.html"], 
+    ["hãy yêu một người xứng đáng là đàn ông", "sachhayyeumotnguoidanong.html"],
+    ["anh chính là thanh xuân em ", "sachAnhChinhLaThanhXuanCuaem.html"],
+    ["ngũ ngon hẹn mai nhé", "sachNguNgonHenMaiNhe.html"],
+    ["cỗ tích của người điên", "sachCotichCuaNguoiDien.html"],
+    ["ánh sáng vô hình", "sachCotichCuaNguoiDien.html"],
+    ["kẻ nhìn trộm", "sachKeNhinTrom.html"],
+    ["mười người da đen đỏ", "sachMuoiTenĐAenDo.html"],
+    ["bắt trẻ đồng xanh", "sachBAtTreDongXanh"],
+    ["dù sao cũng là chúng ta tuổi 20", "sachDuSAoChungTa.html"],
+    ["lời chia tay đẹp nhất thế gian", "sachLoichiaTAyDep"],
+    ["một lít nước mắt", "sachMotLitNuocMat"],
+];
+
+$(document).ready(function() {
+    $("#btnTimKiem").click(function() {
+        var value = $("#txtTimKiem").val();
+        var trang = "";
+
+        // tim trong mang database du lieu duoc nhap
+        for (var i = 0; i <= database.length; i++) {
+            if (value == database[i][0]) {
+                trang = database[i][1];
+                break;
+            }
+        }   
+        if (trang != "") {
+            $("#iframe").attr("src", trang);
+        }
+    });
+});
