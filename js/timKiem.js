3232aff45e7a564d95d576636d0afb705d57e8b1
nguoicuagiono <51014173+nguoicuagiono@users.noreply.github.com>
2019-06-15 13:41:32 +0700

    Add files via upload

--- js/timKiem.js
+++ js/timKiem.js
@@ -1,16 +1,16 @@
 var database = [
-    ["yêu anh hơn Cả tử thần", "sachyeuanh.html"], 
+    ["yêu anh hơn cả tử thần", "sachyeuanh.html"], 
     ["hãy yêu một người xứng đáng là đàn ông", "sachhayyeumotnguoidanong.html"],
     ["anh chính là thanh xuân em ", "sachAnhChinhLaThanhXuanCuaem.html"],
-    ["ngũ ngon hẹn mai nhé", "sachNguNgonHenMaiNhe.html"],
-    ["cỗ tích của người điên", "sachCotichCuaNguoiDien.html"],
+    ["ngủ ngon hẹn mai nhé", "sachNguNgonHenMaiNhe.html"],
+    ["cổ tích của người điên", "sachCotichCuaNguoiDien.html"],
     ["ánh sáng vô hình", "sachCotichCuaNguoiDien.html"],
     ["kẻ nhìn trộm", "sachKeNhinTrom.html"],
-    ["mười người da đen đỏ", "sachMuoiTenĐAenDo.html"],
+    ["mười người da đen đỏ", "sachmuoitendaden.html"],
     ["bắt trẻ đồng xanh", "sachBAtTreDongXanh.html"],
     ["dù sao cũng là chúng ta tuổi 20", "sachDuSAoChungTa.html"],
     ["lời chia tay đẹp nhất thế gian", "sachLoichiaTAyDep.html"],
-    ["một lít nước mắt", "sachMotLitNuocMat.html"],
+    ["một lít nước mắt", "sachMotLitNuocMat.html"]
 ];
 
 $(document).ready(function() {
